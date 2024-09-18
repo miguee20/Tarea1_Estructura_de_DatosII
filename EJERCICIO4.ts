353a63a6ac73716ad4b8037426e861619b5bd6bc
miguee20 <124699281+miguee20@users.noreply.github.com>
2024-09-17 19:59:07 -0600

    Inciso #4

---
+++ EJERCICIO4.ts
@@ -0,0 +1,12 @@
+// for para repetir 5 veces
+for (let i = 1; i <= 5; i++) {
+    console.log(`REPETIR NUMERO ${i}`);
+}
+
+// while para repetir mientras el contador sea mmenor o igual que 5
+let contador: number = 1;
+
+while (contador <= 5) {
+    console.log(`REPETIR NUMERO ${contador}`);
+    contador++;
+}    
