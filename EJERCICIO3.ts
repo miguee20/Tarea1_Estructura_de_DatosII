fe4bd7646bc485a86f2d1ab654c5d5c67ed61cd4
miguee20 <124699281+miguee20@users.noreply.github.com>
2024-09-17 19:54:48 -0600

    Inciso #3

---
+++ EJERCICIO3.ts
@@ -0,0 +1,9 @@
+let age: number = 18; 
+
+if (age < 18) {
+    console.log("es menor de edad");
+} else if (age >= 18 && age < 65) {
+    console.log("es mayor de edad");
+} else {
+    console.log("es de tercera edad");
+}
