9737ed1fb66af797bb04b9d804a60c0f31f9d824
miguee20 <124699281+miguee20@users.noreply.github.com>
2024-09-17 19:45:23 -0600

    Inciso #!

---
+++ EJERCICIO1.ts
@@ -0,0 +1,9 @@
+// Declare variables de diferentes tipos (number, string, boolean) y muestre su valor por consola
+let age: number = 18;
+let name1: string = "Miguel Salguero";
+let isStudent: boolean = true;
+
+console.log("La edad es:", age);
+console.log("La edad es:", name1);
+console.log("La edad es:", isStudent);
+
