499c75c5781041c75abcf25e7484bd878233e91a
miguee20 <124699281+miguee20@users.noreply.github.com>
2024-09-17 19:50:43 -0600

    Inciso #2

---
+++ EJERCICIO2.ts
@@ -0,0 +1,18 @@
+let num1: number = 10;
+let num2: number = 3;
+
+let suma = num1 + num2;  
+
+let resta = num1 - num2;  
+
+let multiplicacion = num1 * num2;
+
+let division = num1 / num2;   
+
+let modulo = num1 % num2;         
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicacion:", multiplicacion);
+console.log("Division:", division);
+console.log("Modulo:", modulo);
